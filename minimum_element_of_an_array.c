11fbe6f10ed5716059333098f97e5099cd8504ca
22MH1A04I3 <119720080+22MH1A04I3@users.noreply.github.com>
2022-12-10 11:52:23 +0530

    #include<stdio.h> int main() {     int N;     scanf("%d",&N);     int a[N],i;     for(i=0;i<N;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<N;i++)     {        if(a[0]>a[i])        {        a[0]=a[i];        }     }     printf("%d",a[0]); }

---
+++ minimum_element_of_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int a[N],i;
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+       if(a[0]>a[i])
+       {
+       a[0]=a[i];
+       }
+    }
+    printf("%d",a[0]);
+}
